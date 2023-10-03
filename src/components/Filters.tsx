c46c3236d1eb9709b7410a03ab51005c71490be4
Jan Gross <jan.gross.cze@gmail.com>
2023-10-03 09:32:50 +0200

    radio button edit

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,29 +1,29 @@
 
 const Filters = () => {
   return (
-    <div className="filters bg-cyan-400 p-2 flex flex-col w-[20%] m-2 h-20vh">
+    <div className="filters bg-blue-400 p-2 flex flex-col w-[20%] m-2 h-20vh">
         <span className="filter-title">Filtr</span>
 
         {/* Rdio buttons - for prices */}
         <span>
-           <input type="radio"/>
-           <label htmlFor="">od nejlevnějších</label>
+           <input type="radio" id="descending" name="price"/>
+           <label htmlFor="descending">nejlevnější</label>
         </span>
         <span>
-              <input type="radio"/>
-              <label htmlFor="">od nejdražších</label>
+              <input type="radio" id="ascending" name="price"/>
+              <label htmlFor="ascending">nejdražší</label>
         </span>
 
         {/* Checkboxes for product filtering */}
         <span>
-            <input type="checkbox"/>
-            <label htmlFor="">skladem</label>
+            <input type="checkbox" id="inStock"/>
+            <label htmlFor="inStock">skladem</label>
         </span>
         <span>
-            <input type="checkbox"/>
-            <label htmlFor="">rychlé doručení</label>
+            <input type="checkbox" id="fastDelivery"/>
+            <label htmlFor="fastDelivery">rychlé doručení</label>
         </span>
-
+        <button>Reset</button>
     </div>
   )
 }
