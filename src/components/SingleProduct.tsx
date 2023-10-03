89603fe428ac5a5be81fa2a57b51e14fe7db8de2
Jan Gross <jan.gross.cze@gmail.com>
2023-10-03 11:30:50 +0200

    basic structure for SingleProduct component

--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -1,12 +1,20 @@
-
+import Rating from "./Rating"
 
 const SingleProduct = ({ prod }) => {
     const {id, name, image, price, fastDelivery, ratings, offer, inStock} = prod
   return (
-    <div key={id}>
-       {/* <img src={image} alt="" /> */}
+    <div key={id} className="">
+       <img src={image} alt="" className="w-32"/>
        <p>{name}</p>
-         
+       <span>{price}</span>
+       {fastDelivery ? (
+        <div>Fast Delivery</div>
+        ) : (
+        <div>4 days delivery</div>
+        )}
+        <span>
+            <Rating rating={ratings}/>
+        </span>
         
     </div>
 )}
