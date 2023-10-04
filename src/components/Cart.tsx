4f45c2996ced74e2f1029387bc0fe2f2966fb293
Jan Gross <jan.gross.cze@gmail.com>
2023-10-04 15:09:12 +0200

    new reducer for changing quality and price in CART

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import { AiFillDelete } from "react-icons/ai"
 import { CartState } from "../context/Context"
 import {useState, useEffect} from "react"
+import { Link } from "react-router-dom"
 
 
 const Cart = () => {
@@ -12,7 +13,7 @@ const Cart = () => {
   useEffect(() => {
   // reduce mi vytáhne z objektu cenu
   const reduce = cart.reduce((accumulator, currentValue) => 
-     accumulator + Number(currentValue.price),0)
+     accumulator + Number(currentValue.price) * currentValue.qty,0)
   setTotal(reduce)
   }, [cart])
 
@@ -27,25 +28,28 @@ const Cart = () => {
               <div key={id} className="oneProduct grid grid-cols-5">
                   <img src={image} alt="" className="w-40"/>
                   <p>{name}</p>
-                  <p>{price}</p>
 
                   {/* qty will change by inStock variable  */}
                   <span>
                     <form action="">
                       <label htmlFor="quantity">
-                        <select name="quantity" id="quantity">
-                          {[...Array(inStock).keys()].map((x) => (
-                            <option value={x+1}>{x+1}</option>
-                          )
-                          )}
+                        <select 
+                          name="quantity" 
+                          id="quantity" 
+                          onChange={(e) => dispatch({ type: "CHANGE_QTY", payload: { id: prod.id, qty:e.target.value}})} >
+                            {[...Array(inStock).keys()].map((x) => (
+                              <option value={x+1}>{x+1}</option>
+                            )
+                            )}
                         </select>
                       </label>
                     </form>
                   </span>
+
+                  <p>{price} USD</p>
                   <button onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload: prod})}>
                     <AiFillDelete />
                   </button>
-
               </div>
             )})
           }
@@ -57,6 +61,9 @@ const Cart = () => {
           <button></button>
         </div>
      </div>
+     <Link to="/">
+        <button >Zpět na úvodní stranu</button>
+     </Link>
     </>
   )
 }
