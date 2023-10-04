4b344381a6a6178d5ede84121e83f10d46551e63
Jan Gross <jan.gross.cze@gmail.com>
2023-10-04 08:24:18 +0200

    basic styles for DROPDOWNMENU

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -6,14 +6,14 @@ const DropdownMenu = () => {
   const {state: { cart }} = CartState()
   console.log(cart)
   return (
-    <div className="w-[340px] h-[80px] bg-blue-200 float-right border-2 border-black z-100 absolute right-8">
+    <div className="w-[500px] min-h-[100px] bg-blue-100 float-right border-2 border-black z-100 absolute right-8">
       {cart.length > 0 ? (
         cart.map((cart) => 
           (
-           <div key={cart.id} className="flex">
-              <img src={cart.image} className="" />
+           <div key={cart.id} className="flex justify-between items-center p-2">
+              <img src={cart.image} className="w-24 rounded-md" />
               <span>{cart.name}</span>
-              <span>{cart.price}</span>
+              <span className="mr-3 font-bold">{cart.price} USD</span>
             </div>
           ))
         ) : (
