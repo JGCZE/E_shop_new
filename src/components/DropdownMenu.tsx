036818dd4559cf2ef3a10e6015d911cb104ebae0
Jan Gross <jan.gross.cze@gmail.com>
2023-10-04 08:49:36 +0200

    updated DROPDOWN - remove items from Cart. Updated variables

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,19 +1,23 @@
+import { AiFillDelete } from "react-icons/ai"
 import { CartState } from "../context/Context"
 import Cart from "./Cart"
 
 
 const DropdownMenu = () => {
-  const {state: { cart }} = CartState()
-  console.log(cart)
+  const {state: { cart }, dispatch} = CartState()
+
   return (
     <div className="w-[500px] min-h-[100px] bg-blue-100 float-right border-2 border-black z-100 absolute right-8">
       {cart.length > 0 ? (
-        cart.map((cart) => 
+        cart.map((prod) => 
           (
-           <div key={cart.id} className="flex justify-between items-center p-2">
-              <img src={cart.image} className="w-24 rounded-md" />
-              <span>{cart.name}</span>
-              <span className="mr-3 font-bold">{cart.price} USD</span>
+           <div key={prod.id} className="flex justify-between items-center p-2">
+              <img src={prod.image} className="w-24 rounded-md" />
+              <span>{prod.name}</span>
+              <span className="mr-3 font-bold">{prod.price} USD</span>
+              <button onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload: prod})}>
+                <AiFillDelete className="text-2xl text-red-500"/>
+              </button>
             </div>
           ))
         ) : (
