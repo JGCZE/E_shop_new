73cb951ab4a172427e4444e03ddac429d8bdfbb8
Jan Gross <jan.gross.cze@gmail.com>
2023-10-05 17:16:14 +0200

    FILTERS updated styles

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -9,11 +9,12 @@ const Filters = () => {
     // const [rating, setRating] = useState(2) nahrazeno "byRaing" z Context.tsx
 
   return (
-    <div className="filters bg-blue-400 p-2 flex flex-col min-w-[250px] m-4 h-[600px] ">
-        <h3 className="filter-title text-center text-xl m-6 font-semibold">Filtr produktů</h3>
+    <div className="filters bg-blue-400 p-2 flex flex-col min-w-[250px] m-4 h-[600px]">
+        <h3 className="filter-title text-center text-2xl mt-6 mb-10 font-semibold">Filtr produktů</h3>
 
         {/* Rdio buttons - for prices - only one is possible */}
-        <span className="mt-4">
+        <span className="font-bold ">Dle ceny</span>
+        <span className="">
            <input type="radio" id="descending" name="price" 
                   onChange={() => productDispatch({ type: "SORT_BY_PRICE", payload: "lowToHigh"})}
                   checked={sort === "lowToHigh" ? true : false}
@@ -29,12 +30,13 @@ const Filters = () => {
         </span>
 
         {/* Checkboxes for product filtering - all is possible */}
+        <span className="mt-10 font-bold">Podle dostupnosti</span>
         <span>
             <input type="checkbox" id="inStock" 
                    onChange={() => productDispatch({ type: "FILTER_BY_STOCK"})}
                    checked={byStock}
             />
-            <label htmlFor="inStock">skladem</label>
+            <label htmlFor="inStock" className="checked:font-bold">skladem</label>
         </span>
         <span>
             <input type="checkbox" id="fastDelivery" 
@@ -51,7 +53,7 @@ const Filters = () => {
         </span>
             
         {/* Reset button */}
-        <button className="bg-white py-2 rounded-md text-black mt-6 text-lg" 
+        <button className="bg-white hover:bg-slate-100 py-2 rounded-md text-black mt-6 text-lg" 
                 onClick={() => productDispatch({ type: "CLEAR_FILTERS" })}
         >
             Reset
