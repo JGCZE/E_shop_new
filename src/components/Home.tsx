3c8b86d9175f9c003795fdf698c32c6f03b62968
Jan Gross <jan.gross.cze@gmail.com>
2023-10-05 08:00:25 +0200

    added values to HOME for using filtr. First filter by price. Maping function instead of array

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,14 +7,25 @@ const Home = () => {
 
   // access the products array from Context.tsx
   // destructure the products array from the state object
-  const { state : { products }} = CartState();
+  const { state : { products }, productState: { byStock, byRating, sort, byFastDelivery, searchQuery }} = CartState();
 
+  const transformedProducts = () => {
+    let sortedProducts = products;
+
+    // sorting by price
+    if(sort) {
+      sortedProducts = products.sort((a,b) => a.price - b.price)
+    }
+    //console.log(products.sort((a,b) => a.price - b.price))
+    return sortedProducts
+  }
+  
 
   return (
     <div className="home flex">
      <Filters />
      <div className="productsContainer flex flex-wrap justify-around w-[78%] p-4 gap-4">
-        {products.map((prod ) => {
+        {transformedProducts().map((prod ) => {
             return (
               <SingleProduct prod={ prod }/>
             )
